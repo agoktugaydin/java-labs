db12ca37f1dbe6b1d3e20e1314b5b15b93cb4c6c
agoktugaydin <goktugaydin36@gmail.com>
2022-11-02 00:58:39 +0300

    lab1

---
+++ Lab1/main.java
@@ -0,0 +1,10 @@
+
+public class main {
+    
+    public static void main(String args[]) 
+    {
+    System.out.println("argument that you entered is: ");
+    System.out.println(args[0]);
+    }
+}
+
